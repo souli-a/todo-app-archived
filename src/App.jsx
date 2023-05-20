a67313786ddf698c8520fd78c9dfb723eb686eb7
souli-a <soulimane.a@protonmail.com>
2023-05-20 19:31:47 +0200

    feat: add sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,84 @@
-const App = () => {
- return (
-    <div>
-    </div>
+import styled from 'styled-components';
+import * as Form from '@radix-ui/react-form';
+import { colors } from './Colors.jsx';
+
+const FormRoot = styled(Form.Root)`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  color: ${colors.black};
+  font-weight: 300;
+`;
+
+const FormField = styled(Form.Field)`
+  width: 100%;
+`;
+
+const FormLabel = styled(Form.Label)`
+  font-size: 2rem;
+`;
+
+const Input = styled.input`
+  font-size: 2rem;
+  margin-top: 10px;
+  border: 1px solid ${colors.grey2};
+  border-radius: 3px;
+  padding: 0.9rem 0rem;
+  width: 100%;
+  outline: none;
+  &:focus {
+    box-shadow: 0 0 0 2px ${colors.grey1};
+  }
+`;
+
+const Button = styled.button`
+  font-size: 2rem;
+  background-color: ${colors.green3};
+  color: ${colors.black};
+  border: none;
+  padding: 0.9rem 1.8rem;
+  border-radius: 3px;
+  margin-top: 10px;
+  width: 100%;
+  font-weight: 500;
+  &:hover {
+    background-color: ${colors.green2};
+  }
+  &:active {
+    background-color: ${colors.green1};
+  }
+  &:focus {
+    box-shadow: 0 0 0 2px ${colors.green5};
+  }
+`;
+
+const SignUp = () => {
+  return (
+    <FormRoot>
+      <FormField name="email">
+        <FormLabel>Email</FormLabel>
+        <Form.Control asChild>
+          <Input type="email" />
+        </Form.Control>
+      </FormField>
+      <FormField name="password">
+        <FormLabel>Mot de passe</FormLabel>
+        <Form.Control asChild>
+          <Input type="password" />
+        </Form.Control>
+      </FormField>
+      <FormField name="confirmPassword">
+        <FormLabel>Confirmer le mot de passe</FormLabel>
+        <Form.Control asChild>
+          <Input type="password" />
+        </Form.Control>
+      </FormField>
+      <Form.Submit asChild>
+        <Button>S'inscrire</Button>
+      </Form.Submit>
+    </FormRoot>
   );
 };
 
-export default App;
+export default SignUp;
---
+++ src/Colors.jsx
@@ -0,0 +1,22 @@
+export const colors = {
+  black: '#222222',
+  white: '#f3f3f3',
+
+  red1: '#f44336',
+  red2: '#f5554a',
+  red3: '#f6685e',
+  red4: '#f77b72',
+  red5: '#f88e86',
+
+  green1: '#8be64a',
+  green2: '#96e85c',
+  green3: '#a2eb6e',
+  green4: '#aded80',
+  green5: '#b9f092',
+
+  grey1: '#eeeeee',
+  grey2: '#d6d6d6',
+  grey3: '#bebebe',
+  grey4: '#a6a6a6',
+  grey5: '#8e8e8e',
+};
