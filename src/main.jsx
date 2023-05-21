6384ffb24d547ab53ae0bd2d394414ed46d2a05c
souli-a <soulimane.a@protonmail.com>
2023-05-21 20:06:44 +0200

    feat(ui): add header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TermsAndConditions from './TermsAndConditions.jsx';
+import Header from './Header';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TermsAndConditions />
+    <Header />
   </React.StrictMode>
 );
